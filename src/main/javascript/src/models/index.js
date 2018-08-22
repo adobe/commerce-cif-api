4f20cd3b82b078ce87b17b7ea081310b9530367c
Christophe Jelger <jelger@adobe.com>
2018-08-22 10:51:27 +0200

    CIF-419 - CIF API generation code should use templates (#36)
    - created Handlebars templates to generate the JS models and Nginx configuration
    - moved code generation to src/models and src/nginx

---
+++ src/main/javascript/src/models/index.js
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ *
+ *    Copyright 2018 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+const genFolder = __dirname + '/../../../resources/generated';
+const jsFolder = genFolder + '/javascript';
+const fs = require('fs');
+const _ = require('lodash');
+const handlebars = require('handlebars');
+const swagger = require(genFolder + '/swagger/swagger.json');
+const generateModelClass = require(__dirname + '/model.js').generateModelClass;
+
+if (!fs.existsSync(jsFolder)) {
+    fs.mkdirSync(jsFolder);
+}
+
+// Generate package.json
+let modelsPackageJson = fs.readFileSync(__dirname + '/models-package.json', 'utf-8').replace('VERSION_PLACEHOLDER', escape(swagger.info.version));
+let packageJsonFile = fs.openSync(jsFolder + '/package.json', 'w');
+fs.appendFileSync(packageJsonFile, modelsPackageJson);
+
+let classes = [];
+// Generates all JS model classes
+_.forEach(swagger.definitions, function(definition, classname) {
+    classes.push(classname);
+    let model = fs.openSync(jsFolder + '/' + classname + '.js', 'w');
+    fs.appendFileSync(model, generateModelClass(swagger, definition, classname));
+});
+
+// Generate index file for model package
+let indexTemplate = fs.readFileSync(__dirname + '/index.hbs', 'utf8');
+let compiledTemplate = handlebars.compile(indexTemplate, {noEscape: true});
+let index = fs.openSync(jsFolder + '/index.js', 'w');
+fs.appendFileSync(index, compiledTemplate({version: swagger.info.version, classes: classes}));
+
+// Add OW package name to all "operationId" fields
+replaceOperationIdProperties();
+
+// We do a final change to the Swagger spec, by adding the Openwhisk package name to all "operationId" fields
+// This is required for the 'wsk api create' configuration of the REST API in Adobe I/O Runtime
+function replaceOperationIdProperties() {
+    let pkg = swagger.info['x-ow-package'] || 'default';
+
+    let swagJson = fs.readFileSync(genFolder + '/swagger/swagger.json', 'utf-8');
+    swagJson = swagJson.replace(/"operationId" : "/g, '"operationId" : "' + pkg + '/');
+    fs.writeFileSync(genFolder + '/swagger/swagger.json', swagJson);
+
+    let swagYaml = fs.readFileSync(genFolder + '/swagger/swagger.yaml', 'utf-8');
+    swagYaml = swagYaml.replace(/operationId: "/g, 'operationId: "' + pkg + '/');
+    fs.writeFileSync(genFolder + '/swagger/swagger.yaml', swagYaml);
+}
--- src/main/javascript/src/nginx/index.js
+++ src/main/javascript/src/nginx/index.js
@@ -11,3 +11,17 @@
  *    governing permissions and limitations under the License.
  *
  ******************************************************************************/
+
+const genFolder = __dirname + '/../../../resources/generated/nginx';
+const _ = require('lodash');
+const fs = require('fs');
+const escape = require('js-string-escape');
+const swagger = require(__dirname + '/../../../resources/generated/swagger/swagger.json');
+const generateNginxConfig = require(__dirname + '/nginx.js').generateNginxConfig;
+
+if (!fs.existsSync(genFolder)) {
+    fs.mkdirSync(genFolder);
+}
+
+let schema = fs.openSync(genFolder + '/nginx-config.txt', 'w');
+fs.appendFileSync(schema, generateNginxConfig(swagger));
